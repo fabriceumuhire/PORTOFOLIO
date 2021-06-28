5e6a3eaee7614a53bd9c75d031f4892a91fabdae
fabriceumuhire <fabrice.umuhire.ubugeni@gmail.com>
2021-06-28 10:50:59 +0200

    feature for adding user queries

--- backend/src/middlewares/user.middleware.js
+++ backend/src/middlewares/user.middleware.js
@@ -5,11 +5,11 @@ import User from '../models/user.model';
 export const Authenticate = async (req, res, next) => {
   const user = await User.findOne({ email: req.body.email });
   if (!user) {
-    return res.status(401).send('Wrong email');
+    return res.status(401).json({ message: 'Wrong email' });
   }
   const pass = await bcrypt.compare(req.body.password, user.password);
   if (!pass) {
-    return res.status(401).send('Wrong password');
+    return res.status(401).json({ message: 'Wrong password' });
   }
   next();
 };
